f0283cb299425d9044360f1d19f144db5234aa10
AabidMulani <mulaniaabid@gmail.com>
2014-04-22 22:15:42 +0530

    Sound settings On-Off done.

--- MenuScreen.lua
+++ MenuScreen.lua
@@ -1,37 +1,35 @@
- 
+
 local playButton
 local highScoreButton
 local howToPlayButton
-local rateUsButton
+local soundsButton
 local storyboard= require ( "storyboard" )
 local scene= storyboard.newScene()
 local onButtonClick
 local initialAnimation
 
---On saveButton Clicked
-
-
-
---IMPORTANT
 function scene:createScene(event)
     local group=self.view
     local background = display.newImageRect( "menu_images/menu_screen_background.png",_W,_H)
     background.x=_W/2;
     background.y=_H/2
+    group:insert(background);
     
     local birdImg = display.newImage( "menu_images/bird_1_img.png")
     birdImg.x=_W/2-110;
     birdImg.y=35
+    group:insert(birdImg);
     
     
     local labelScoreTxt= display.newText("High Scores: ", _W/2, _H-110 , system.native, 12)
     labelScoreTxt.x=_W/2
     labelScoreTxt.y=_H-80
     labelScoreTxt:setTextColor(255,255,255)
+    group:insert(labelScoreTxt);
     
-        local highScores=getHighScore()
+    local highScores=getHighScore()
     if(highScores==nil)then
-   	 saveHighScore(0)
+        saveHighScore(0)
     	HIGH_SCORE=0;
     else
     	HIGH_SCORE=highScores
@@ -41,13 +39,14 @@ function scene:createScene(event)
     highScoreTxt.x=_W/2
     highScoreTxt.y=_H-75+labelScoreTxt.height  
     highScoreTxt:setTextColor(255,255,255)
+    group:insert(highScoreTxt);
     
-
+    
     
     local coinsValue=getCoinsCounts()
     
     if(coinsValue==nil)then
-   	 saveCoinsCounts(201)
+        saveCoinsCounts(201)
     	AVAILABLE_COINS=200;
     else
     	AVAILABLE_COINS=tostring(coinsValue)
@@ -57,17 +56,14 @@ function scene:createScene(event)
     coinsTxt.x=_W/2
     coinsTxt.y=_H-30 
     coinsTxt:setTextColor(255,255,255)
+    group:insert(coinsTxt);
     
     local labelCoinsImg= display.newImage( "menu_images/coins_icon.png")
     labelCoinsImg.x=_W/2-(coinsTxt.width/2)-(labelCoinsImg.width/2)
     labelCoinsImg.xScale=0.6
     labelCoinsImg.yScale=0.6
     labelCoinsImg.y=_H-30
-    
-    --background=display.newRect( 0 , 0, _W, _H )
-    --background.x=_W/2;
-    --background.y=_H/2
-    --background:setFillColor ( 0,0 , 255 ,1 )
+    group:insert(labelCoinsImg);
     
     playButton = display.newImage( "menu_images/play_btn.png")
     playButton.x=_W/2
@@ -76,6 +72,7 @@ function scene:createScene(event)
     playButton.xScale=0.5
     playButton.yScale=0.5
     playButton.filename="GameScreen"
+    group:insert(playButton);
     
     
     highScoreButton = display.newImage( "menu_images/high_score_btn.png")
@@ -85,6 +82,7 @@ function scene:createScene(event)
     highScoreButton.xScale=0.5
     highScoreButton.yScale=0.5
     highScoreButton.filename="HighScoreScreen"
+    group:insert(highScoreButton);
     
     howToPlayButton = display.newImage( "menu_images/how_to_play_btn.png")
     howToPlayButton.x=_W/2
@@ -93,17 +91,38 @@ function scene:createScene(event)
     howToPlayButton.xScale=0.5
     howToPlayButton.yScale=0.5
     howToPlayButton.filename="HowToPlayScreen"
-    
-    group:insert(background);
-    group:insert(birdImg);
-    group:insert(labelScoreTxt);
-    group:insert(highScoreTxt);
-    group:insert(labelCoinsImg);
-    group:insert(coinsTxt);
-    group:insert(playButton);
-    group:insert(highScoreButton);
     group:insert(howToPlayButton);
---    group:insert(rateUsButton);	
+    
+    
+    local function onSoundChange()
+        display.remove(soundsButton)
+        if(IS_SOUND_ON=="true")then
+            IS_SOUND_ON="false"
+            saveSoundSettings("false")
+            soundsButton = display.newImage( "menu_images/sound_off_btn.png")
+        else
+            IS_SOUND_ON="true"
+            saveSoundSettings("true")
+            soundsButton = display.newImage( "menu_images/sound_on_btn.png")
+        end
+        soundsButton.x=15+(soundsButton.width/2)
+        soundsButton.y=15+(soundsButton.width/2)
+        soundsButton:addEventListener ( "tap", onSoundChange )
+        group:insert(soundsButton);
+    end
+    
+    IS_SOUND_ON=getSoundSettings()
+    
+    if(IS_SOUND_ON=="true")then
+        soundsButton = display.newImage( "menu_images/sound_on_btn.png")        
+    else
+        soundsButton = display.newImage( "menu_images/sound_off_btn.png")
+    end
+    
+    soundsButton.x=15+(soundsButton.width/2)
+    soundsButton.y=15+(soundsButton.width/2)
+    soundsButton:addEventListener ( "tap", onSoundChange )
+    group:insert(soundsButton);	
 end
 
 --IMPORTANT
@@ -121,19 +140,19 @@ function scene:enterScene( event )
     	print ( "No User found" )
     	saveUserName("AABID")
     	USER_NAME="AABID"
-    	else
+    else
     	USER_NAME=userName
     end
     
-
     
-	Runtime:addEventListener( "key", onKeyEvent );
+    
+    Runtime:addEventListener( "key", onKeyEvent );
     initialAnimation()
     playButton:addEventListener ( "touch", onButtonClick )
     howToPlayButton:addEventListener ( "touch", onButtonClick )
     highScoreButton:addEventListener ( "touch", onButtonClick )
---    rateUsButton:addEventListener ( "touch", onButtonClick )
-   
+    --    rateUsButton:addEventListener ( "touch", onButtonClick )
+    
 end
 
 --IMPORTANT
@@ -173,10 +192,10 @@ end
 
 function initialAnimation()
     local animSpeed=150
-
---local function showRateUs(event)
---	transition.to( rateUsButton, {time=animSpeed, alpha=1, xScale=1, yScale=1})
---    end
+    
+    --local function showRateUs(event)
+    --	transition.to( rateUsButton, {time=animSpeed, alpha=1, xScale=1, yScale=1})
+    --    end
     
     local function showHowToPlay(event)
 	transition.to( howToPlayButton, {time=animSpeed, alpha=1, xScale=1, yScale=1})
@@ -191,11 +210,11 @@ function initialAnimation()
 end
 
 function onKeyEvent( event )
-
-	if(event.phase == "down" and event.keyName == "back") then
-			
-			 native.requestExit()
-		
+    
+    if(event.phase == "down" and event.keyName == "back") then
+        
+        native.requestExit()
+        
         return true
     end
     return false
@@ -206,7 +225,7 @@ function onButtonClick( event)
     print ( "onButtonClick" )
     print ( event.phase )
     
-Runtime:removeEventListener( "key", onKeyEvent );
+    Runtime:removeEventListener( "key", onKeyEvent );
     local obj= event.target
     print ( obj.filename)
     
@@ -221,7 +240,6 @@ Runtime:removeEventListener( "key", onKeyEvent );
         if(event.phase == "ended" )then
             --call another screen
             
-    		
             options = {effect = "fade",time=300 }
             storyboard.gotoScene(obj.filename,options)
         end
